5ca027113e6c864946a585adcf4659c458f14f38
elisagc <elisagcubero@gmail.com>
2019-11-18 20:34:24 +0100

    puesto en marcha sass

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./sass/main.scss"; // IMPORTAR SASS/MAIN.SCSS
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
