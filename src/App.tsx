53196276809070c285f4d8b08e14e332d1781c36
elisagc <elisagcubero@gmail.com>
2019-11-18 20:53:24 +0100

    añadido router y test router primera parte

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Route } from "react-router";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
 
 const App: React.FC = () => {
-  return <div data-testid="main">Hello from App</div>;
+  return (
+    <div data-testid="main">
+      <Route path="/" component={Home} />
+      <Route path="/rooms/" component={Rooms} />
+      <Route path="/single-room" component={SingleRoom} />
+    </div>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom"; // IMPORTAR BROWSERROUTER  EN INDEX.TSX
 import App from "./App";
 import "./sass/main.scss"; // IMPORTAR SASS/MAIN.SCSS
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
