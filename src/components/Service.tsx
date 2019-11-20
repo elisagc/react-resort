dd2ceafd6297772910ff6fd5ae3386c0d2764ffc
elisagc <elisagcubero@gmail.com>
2019-11-20 16:59:27 +0100

    creados componentes que crean servicios desde api simulada

---
+++ src/components/Service.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Service } from "../types/service";
+
+const ServiceComponent = ({ service }: { service: Service }) => {
+  return (
+    <article className="service" data-testid="service">
+      <span>{service.icon}</span>
+      <h6>{service.title}</h6>
+      <p>{service.info}</p>
+    </article>
+  );
+};
+
+export default ServiceComponent;
